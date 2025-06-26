8bd8a9e00d59abcf356f53cd10965f4c6af5551e
naimaabdi56 <nacimoabdinur@gmail.com>
2025-06-26 22:30:02 +0300

    project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Nacimo Abdinur Mohamud";
+Console.WriteLine("Name: " + name);
+
+string motherName = "Safiyo Ahmed";
+Console.WriteLine("Mother's Name: " + motherName);
+
+DateTime dob = new DateTime(2003, 8, 20);
+Console.WriteLine("Date of Birth: " + dob.ToString("yyyy-MM-dd"));
+
+string pob = "Kismayo";
+Console.WriteLine("Place of Birth: " + pob);
+
+string address = "Israac";
+Console.WriteLine("Address: " + address);
+
+int telephone = 96154536;
+Console.WriteLine("Telephone: " + telephone);
+
+string sex = "Female";
+Console.WriteLine("Sex: " + sex);
+
+string status = "Single";
+Console.WriteLine("Status: " + status);
